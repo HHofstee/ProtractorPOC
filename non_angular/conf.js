1da8abb6c689a3c0c556e4e60c3d2c8bb2151ab1
Harm Hofstee <harm@tallcomponents.com>
2016-07-26 15:24:06 +0200

    non-angular test on chrome and firefox

--- non_angular/conf.js
+++ non_angular/conf.js
@@ -1,8 +1,11 @@
 // conf.js
 exports.config = {
-  framework: 'jasmine',
-  specs: ['spec.js'],
-  capabilities: {
-    browserName: 'phantomjs'
-  }
+	framework: 'jasmine',
+	specs: ['spec.js'],
+	directConnect: true,
+  	multiCapabilities: [{
+	  'browserName': 'firefox'
+	}, {
+	  'browserName': 'chrome'
+	}]
 }
